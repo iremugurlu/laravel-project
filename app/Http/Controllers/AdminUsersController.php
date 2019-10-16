0e293f0434f0088a873a773e753006bc2cf81d4c
iremugurlu <iremugurlu8l@gmail.com>
2019-10-16 12:55:51 +0200

    add user page feature without photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 
 class AdminUsersController extends Controller
@@ -13,8 +16,9 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        //
-        return view('admin.users.index');
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -24,8 +28,9 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        //
-        return view('admin.users.create');
+        $roles = Role::pluck('name', 'id') -> all();
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -34,9 +39,12 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+        User::create($request->all());
+        return redirect('/admin/users');
+
+        //return $request->all();
     }
 
     /**
@@ -58,7 +66,6 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        //
         return view('admin.users.edit');
     }
 
