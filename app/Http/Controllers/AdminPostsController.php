78600812acd2dcc0632b74d7d981d79442ff04b8
iremugurlu <iremugurlu8l@gmail.com>
2019-11-10 20:23:10 +0100

    add pagination

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -14,7 +14,7 @@ class AdminPostsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        $posts = Post::all();
+        $posts = Post::paginate(3);
         return view('admin.posts.index', compact('posts'));
     }
     /**
